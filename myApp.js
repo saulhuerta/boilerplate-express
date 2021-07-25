0404fc3e4ec630e56c1f232362fbf524d4c0693d
saul.huerta <saul.huerta@unosquare.com>
2021-07-24 22:24:34 -0500

    Add body-parser and format

--- myApp.js
+++ myApp.js
@@ -1,76 +1,82 @@
-var express = require('express');
+var express     = require('express');
+var bodyParser  = require('body-parser');
 var app = express();
 
 require('dotenv').config();
 
-app.use("/", function(req, res, next){
-    console.log(req.method + " " + req.path + " - " + req.ip);
-    next();
-});
-  
 
 //Mount the express.static() middleware  to the path /public with app.use(). 
-app.use("/public/", express.static( __dirname + "/public/"));
+app.use( "/public/", express.static( __dirname + "/public/"));
 
-app.get("/:word/echo", (req, res) => {
-    
-    res.json({
-      echo: req.params.word
-    });
+app.use( bodyParser.urlencoded({extended: false}) );
 
-  });
-  
+app.use( "/", function(req, res, next){
 
-app.get("/now", (req, res, next) => {
-    
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+
+});
+
+
+app.get( "/now", function(req, res, next){
+
     req.time = new Date().toString();
     next();
 
-  }, function(req, res){
-    
-    var time = new Date().toString();
+},
+    function(req, res) {
+
+        res.send({"time": req.time});
+
+    }
+);
+
+app.get( "/:word/echo", (req, res) => {
+
     res.json({
-      time: time
+        echo: req.params.word
     });
-    
+
 });
   
 
-app.route("/name")
+app.route( "/name" )
 .get( (req, res) => {
+
     res.json({
         name: req.query.first + ' ' + req.query.last
     });
+
 })
 .post( (req, res) => {
+
     res.json({
-        name: req.query.first + ' ' + req.query.last
+        name: req.body.first + ' ' + req.body.last
     });
+
 });
 
-app.get("/json", (req, res) => {
+app.get( "/json", (req, res) => {
 
-    var response = "Hello json";
-
-    console.log(process.env.MESSAGE_STYLE + "");
-    console.log(response);
-
-    if(process.env.MESSAGE_STYLE === "uppercase"){
+    const   mySecret = process.env['MESSAGE_STYLE'];
+    var     response = "Hello json";
+  
+    console.log(mySecret)
+  
+    if(mySecret === "uppercase"){
         response = "Hello json".toUpperCase();
-    }else{
-        response = "Hello json";
     }
 
-    console.log(response);
-
     res.json({
-      message: response
+        message: response
     });
-  });
+
+});
+    
 
   
 //Use the app.get() method to serve all GET requests matching the / (root) path.
-app.get('/', function (req, res) {
+app.get( "/", function (req, res) {
     
     //res.send('Hello Express')   //Send 'Hello Express'
 
@@ -78,23 +84,6 @@ app.get('/', function (req, res) {
      res.sendFile ( __dirname + "/views/index.html"); 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
